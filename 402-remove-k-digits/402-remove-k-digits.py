868a524db9350987e084a699c7c768046d53197d
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-07-18 17:52:07 -0700

    Time: 70 ms (93.31%), Space: 15.5 MB (41.78%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.py
+++ 402-remove-k-digits/402-remove-k-digits.py
@@ -1,26 +1,16 @@
 class Solution:
     def removeKdigits(self, nums: str, k: int) -> str:
-        #monotonic stack increasing 
-        if len(nums)==1: # if only 1 digit we can only get 0 
-            return "0"
-
-        stack = []
-        
-        for num in nums:
-            # k--> no of deletion
-            # len(nums)-k --> len of our answer --> len of stack at last
-            while stack and stack[-1]>num and k>0:
-                stack.pop()
-                k-=1
-            stack.append(num)
-
-        # a case like [112] k = 1    no deletion will happen because there is no valley
-        # so we pop k elements from last
+        if len(nums)==1: return "0"
+        stack = [];
+        for i in nums:
+            while(k>0 and stack and stack[-1]>i):
+                stack.pop();
+                k-=1;
+            stack.append(i);
         while k>0 and stack:
-            stack.pop()
+            stack.pop();
             k-=1
- 
-        return ''.join(stack).lstrip("0") or "0"
+        return "".join(stack).lstrip("0") or "0"
 
 
         
