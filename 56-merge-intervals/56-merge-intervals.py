c1b301a474e2242988c92bac5ee8662a5ffb56bd
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-07-19 13:21:19 -0700

    Time: 265 ms (28.81%), Space: 18.9 MB (25.29%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -3,16 +3,15 @@ class Solution:
         if len(nums)==0: return 
         if len(nums)==1: return nums
         nums.sort(key = lambda x:x[0])
-        
-        stack =[];
-        for i in nums:
-            if not stack:
-                stack.append(i);
-            if stack[-1][1]<i[0]:
-                stack.append(i);
+        ans = [nums[0]];
+        for start,end in nums[1:]:  #LIEV --> Last Input's Ending Value
+            LIEV = ans[-1][1];
+            if start<=LIEV:
+                ans[-1][1] = max(LIEV,end)
             else:
-                stack[-1][1] = max(i[1] , stack[-1][1])
-        return stack
+                ans.append([start,end])
+        return ans
+        
             
        
         
