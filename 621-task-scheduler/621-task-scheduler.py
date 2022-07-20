884186801206b24cb6260cc8ed6acbf7e461de98
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-07-20 10:01:09 -0700

    Time: 1144 ms (16.67%), Space: 14.4 MB (39.73%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.py
+++ 621-task-scheduler/621-task-scheduler.py
@@ -4,17 +4,19 @@ class Solution:
         c=Counter(tasks)
         maxHeap = [-i for i in c.values()]
         heapq.heapify(maxHeap)
-        print(maxHeap)
         q=deque();
-        curr_time = 0;
+        t = 0;
         while maxHeap or q:
-            curr_time+=1;
+            t+=1;
             if maxHeap:
-                count = 1+heapq.heappop(maxHeap)
-                if count:
-                    q.append([count,curr_time+n])
-            if q and q[0][1]==curr_time:
-                heapq.heappush(maxHeap,q.popleft()[0])
-        return curr_time
-        
+                MFE = 1+heapq.heappop(maxHeap)#Most Frequent Element right now
+                if MFE!=0:
+                    q.append([MFE , t+n])
+            if q and q[0][1] == t:
+
+                heapq.heappush(maxHeap, q.popleft()[0])
+                
+        return t
+    
+            
         
