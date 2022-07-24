0e8dac3f8d68cc3f816db00ad73543a1a83b2863
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-07-24 04:41:20 -0700

    Time: 122 ms (6.17%), Space: 14 MB (81.06%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.py
+++ 948-bag-of-tokens/948-bag-of-tokens.py
@@ -1,20 +1,26 @@
 class Solution:
-    def bagOfTokensScore(self, tokens: List[int], P: int) -> int:
+    def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
         tokens.sort()
-        l = 0
-        r = len(tokens)-1
-        ans = 0
-        score = 0
-        while r >= l:
-            if P >= tokens[l]:
-                score += 1
-                P -= tokens[l]
-                l += 1
-                ans = max(ans, score)
-            elif P < tokens[l] and score > 0:
-                score -= 1
-                P += tokens[r]
-                r -= 1
+        i = 0;
+        j = len(tokens)-1
+        score = 0;
+        while(i<=j):
+        
+            if power >= tokens[i]:
+                power-=tokens[i]
+                score+=1;
+                i+=1
             else:
-                return ans
-        return ans
+                if power + tokens[j]>=tokens[i] and i!=j and score!=0:
+                    power+=tokens[j]
+                    score-=1;
+                    j-=1
+                else:
+                    break
+        return score
+    '''
+Sort the tokens, l = 0 and r = length - 1
+If there is enough power to flip the token[l], then do it and get 1 point.
+if there is not enough power to flip token[l], then use 1 point to get the token[r] power.
+If can not do both, stop.
+'''
