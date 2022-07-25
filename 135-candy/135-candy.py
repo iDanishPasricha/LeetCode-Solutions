e5459ea2075549d3399fe80e3a28ad690f25b4da
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-07-24 18:30:23 -0700

    Time: 170 ms (94.55%), Space: 17 MB (18.77%) - LeetHub

--- 135-candy/135-candy.py
+++ 135-candy/135-candy.py
@@ -4,7 +4,7 @@ class Solution:
         ans = [1 for _ in range(n)]
         for i in range(1,n):
             if ratings[i]>ratings[i-1]:
-                ans[i] = max(1+ans[i-1] , ans[i])
+                ans[i] = 1+ans[i-1]
         for i in range(n-2,-1,-1):
             if ratings[i]>ratings[i+1]:
                 ans[i] = max(1+ans[i+1] , ans[i])
