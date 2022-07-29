965c7502d028fdcfe9718e112977ad018484ffc3
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-07-29 10:36:41 -0700

    Time: 302 ms (88.72%), Space: 14.8 MB (83.68%) - LeetHub

--- 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.py
+++ 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.py
@@ -1,12 +1,13 @@
 class Solution:
     def uniqueLetterString(self, S: str) -> int:
         d = {i: [-1, -1] for i in ascii_uppercase}
-        print(d)
+
         ans = 0
-        for i, c in enumerate(S):
-            k, j = d[c]
+        for i in range(len(S)):
+            
+            k, j = d[S[i]]
             ans += (i - j) * (j - k)
-            d[c] = [j, i]
+            d[S[i]] = [j, i]
             
             
             
