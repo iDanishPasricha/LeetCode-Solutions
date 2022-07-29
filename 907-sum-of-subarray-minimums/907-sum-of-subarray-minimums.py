ebf23a9564e9eea1dc5eabaf1f60da07ac75e021
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-07-28 17:01:29 -0700

    Time: 595 ms (72.52%), Space: 19 MB (39.51%) - LeetHub

--- 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.py
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.py
@@ -1,23 +1,41 @@
+import math
 class Solution:
     def sumSubarrayMins(self, nums: List[int]) -> int:
-        
-        M = 10**9+7
-        sums = 0
-        nums.append(0) 
+        nums.append(0);
         stack = [-1]
         
-        for i in range(len(nums)):
-           
-            while stack and nums[i] < nums[stack[-1]]:
-                index = stack.pop()
-            
-                left = index-stack[-1]
-                right = i-index
+        modulo = 10**9+7
+        print(modulo)
+        
+        ans = 0;
+        for currIndex in range(len(nums)):
+            while stack and nums[currIndex]<nums[stack[-1]]:
+                indexOfThatnumberJehdaPopKarna = stack.pop()
                 
-                sums += right*left*nums[index]
-            stack.append(i)
+                indexOfThesecondBiggestNumber = stack[-1]
+                
+                left = indexOfThatnumberJehdaPopKarna - indexOfThesecondBiggestNumber
+                
+                right = currIndex - indexOfThatnumberJehdaPopKarna
+                
+                ans+= nums[indexOfThatnumberJehdaPopKarna]*left*right
+            stack.append(currIndex)
+        return (ans%modulo)
+        
+        '''
+        sums = 0
+        arr.append(0) #Sentinel value to pop all elements off the stack
+        stack = [-1]
+        
+        for i2,num in enumerate(arr):
+	      #Mantain a monotone increasing stack
+            while stack and num < arr[stack[-1]]:
+                index = stack.pop()
+                i1 = stack[-1]   # First lesser element to the left
+                left = index-stack[-1]
+                right = i2-index
+                sums += right*left*arr[index]
+            stack.append(i2)
             
         return sums%M
-            
-        
-        
+        '''
