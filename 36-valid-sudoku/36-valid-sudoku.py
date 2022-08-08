131bc7757661cdd3d4def0a4b95403b610c27f3b
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-08 09:33:41 -0700

    Time: 104 ms (88.72%), Space: 13.9 MB (35.50%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValidSudoku(self, b: List[List[str]]) -> bool:
+        rows=defaultdict(set);
+        cols=defaultdict(set);
+        boxes=defaultdict(set);
+        
+        for i in range(9):   
+            for j in range(9):
+                if b[i][j]!=".":   #b[i][j] is the number.
+                    if (b[i][j] in rows[i] or b[i][j] in cols[j] or b[i][j] in boxes[(i//3,j//3)]):
+                        return False;        
+                    rows[i].add(b[i][j]);
+                    cols[j].add(b[i][j]);
+                    boxes[(i//3,j//3)].add(b[i][j]);
+                        
+        return True
+                        
+                        
