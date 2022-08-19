1cc7f0ebd9b3d7e36576249b2b50473b5fd60190
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-19 10:06:11 -0700

    Time: 59 ms (20.01%), Space: 13.9 MB (83.39%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -2,24 +2,34 @@ class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         m=len(matrix)
         n=len(matrix[0])
-        left,top=0,0;
-        right,bottom=n-1,m-1;
+        
+        top_row = 0
+        bottom_row = m-1
+        
+        left_column = 0
+        right_column = n-1
+        
+        
         ans=[];
-        while left<=right and top<=bottom:
-            for i in range(left,right+1):     #--->
-                ans.append(matrix[top][i]);
-            top+=1;     #shrink
-            for i in range(top,bottom+1):     # bottom ja
-                ans.append(matrix[i][right]);
-            right-=1;    #shrink
-            if left>right or top>bottom: break
+        
+        while left_column<=right_column and top_row<=bottom_row:
+            for i in range(left_column,right_column+1):
+                ans.append(matrix[top_row][i]);
+            top_row+=1;
+            for i in range(top_row,bottom_row+1):
+                ans.append(matrix[i][right_column]);
+            right_column-=1;
             
-            for i in range(right,left-1,-1):
-                ans.append(matrix[bottom][i]);
-            bottom-=1;
-            for i in range(bottom,top-1,-1):
-                ans.append(matrix[i][left]);
-            left+=1;
+            if left_column>right_column or top_row>bottom_row: break;
+                
+            for i in range(right_column,left_column-1,-1):
+                ans.append(matrix[bottom_row][i]);
+            bottom_row-=1;
+            for i in range(bottom_row,top_row-1,-1):
+                ans.append(matrix[i][left_column]);
+            left_column+=1;
         return ans
+                
             
+        
                 
