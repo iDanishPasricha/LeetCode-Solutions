558ccb226310b7073379bd3454b00dae66882bcf
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-19 19:10:26 -0700

    Time: 41 ms (88.99%), Space: 13.9 MB (32.84%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -5,6 +5,7 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        
         head = ListNode(0);
         reserve = start = head
         while l1 and l2:
@@ -17,3 +18,15 @@ class Solution:
             start=start.next;
         start.next = l1 or l2;
         return reserve.next
+    
+'''
+    Time complexity : O(n + m)O(n+m)
+
+Because exactly one of l1 and l2 is incremented on each loop iteration, the while loop runs for a number of iterations equal to the sum of the lengths of the two lists. All other work is constant, so the overall complexity is linear.
+
+Space complexity : O(1)O(1)
+
+The iterative approach only allocates a few pointers, so it has a constant overall memory footprint.
+'''
+    
+    
