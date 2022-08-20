cdf5f715d4bb75e1e8671288c519293443a9ffe5
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-19 23:22:57 -0700

    Time: 40 ms (92.62%), Space: 13.9 MB (95.07%) - LeetHub

---
+++ 139-word-break/139-word-break.py
@@ -0,0 +1,12 @@
+
+class Solution:
+    def wordBreak(self, s: str, d: List[str]) -> bool:
+        dp = [False]*(len(s)+1)
+        dp[-1] = True
+        
+        for i in range(len(s),-1,-1):
+            for word in d:
+                if i+len(word)<=len(s) and s[i:i+len(word)]==word:
+                    dp[i] = dp[i+len(word)]
+                if dp[i]: break
+        return dp[0]
