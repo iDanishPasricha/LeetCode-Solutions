25a173143ba952404ee0ed4ffdb21e6f09ff8d5d
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-19 20:38:38 -0700

    Time: 112 ms (81.86%), Space: 18 MB (98.66%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.py
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.py
@@ -2,21 +2,20 @@ import heapq
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
-        self.heap = nums #creating member variables
-        self.k = k   #creating member variables
+        self.heap = nums
+        self.k = k
         
-        #minheap with k largest elements
         heapq.heapify(self.heap)
-      
-        while len(self.heap) > k:
-            heapq.heappop(self.heap);
+        while(len(self.heap)>self.k):
+            heapq.heappop(self.heap)
+        
        
 
         
     def add(self, val: int) -> int:
-        heapq.heappush(self.heap,val)          #yeh apne app  top 3 (k=3) largest banadega je 
-        if len(self.heap)>self.k:  #je number end te judd rea means heap hoke this means starting position bakwaas hai hataao top k largest to last k honge starting no need
-            heapq.heappop(self.heap)
+        heapq.heappush(self.heap,val)
+        if len(self.heap)> self.k:
+            heapq.heappop(self.heap);
         return self.heap[0]
        
         
