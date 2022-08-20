21701da033b8a67342a3aa2c5320549dd5803799
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-19 21:48:34 -0700

    Time: 226 ms (53.18%), Space: 18.8 MB (25.80%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,11 +1,15 @@
 class Solution:
     def merge(self, nums: List[List[int]]) -> List[List[int]]:
-        if len(nums)==0: return 
-        if len(nums)==1: return nums
+        if len(nums)==0:
+            return 
+        if len(nums)==1:
+            return nums
+        
         nums.sort(key = lambda x:x[0])
-        ans = [nums[0]];
-        for start,end in nums[1:]:  #LIEV --> Last Input's Ending Value
-            LIEV = ans[-1][1];
+        ans = [nums[0]]
+        
+        for start,end in nums[1:]:
+            LIEV = ans[-1][1]
             if start<=LIEV:
                 ans[-1][1] = max(LIEV,end)
             else:
