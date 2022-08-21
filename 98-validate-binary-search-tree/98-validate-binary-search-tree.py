271015ffa069d4633063ccc4edf02f8b2ad1fd1c
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-20 20:28:17 -0700

    Time: 85 ms (23.33%), Space: 17 MB (15.39%) - LeetHub

---
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        
+        def inorder(root):
+            if not root: return []
+            return inorder(root.left) + [root.val] + inorder(root.right)
+        
+        #inorder representation means strictly sorted ascending form 
+        
+        arr=inorder(root);
+        for i in range(0,len(arr)-1):
+            if arr[i]>=arr[i+1]: 
+                return False;
+        return True
+    
+    
+    
+    
+    
