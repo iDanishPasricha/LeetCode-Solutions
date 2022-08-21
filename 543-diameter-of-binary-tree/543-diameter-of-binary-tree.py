b8271fcc5d689e711729258454481b28c146fc13
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-21 10:35:53 -0700

    Time: 52 ms (85.35%), Space: 16.4 MB (10.23%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
@@ -13,5 +13,24 @@ class Solution:
         diameter = 0
         height(root)
         return diameter
+    
+    
+    '''
+    class Solution:
+    def maxPathSum(self, root: TreeNode) -> int:
+        def maxPath(root):
+            nonlocal maxSum
+            if not root:
+                return 0
+            
+            left = maxPath(root.left)
+            right = maxPath(root.right)
+            maxSum = max(maxSum, left + right + root.val)
+            return max(left + root.val, right + root.val, 0)
+        
+        maxSum = -math.inf  
+        maxPath(root)
+        return maxSum
+    '''
         
         
