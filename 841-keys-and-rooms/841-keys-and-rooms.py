442be6d7e38d77d0293c690eb9128166828cecfd
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-22 03:47:42 -0700

    Time: 134 ms (16.66%), Space: 14.3 MB (98.97%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.py
+++ 841-keys-and-rooms/841-keys-and-rooms.py
@@ -1,7 +1,7 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
         rooms_visited=[False]*len(rooms); 
-        rooms_visited[0]=1;
+        rooms_visited[0]=True;
         q = deque([0])
         while q:
             curr=q.popleft();
