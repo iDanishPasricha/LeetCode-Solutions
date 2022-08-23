c248e01320adacdcc55240b8717c666d06024ecc
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-23 15:59:08 -0700

    Time: 8174 ms (36.22%), Space: 14.1 MB (57.66%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.py
+++ 16-3sum-closest/16-3sum-closest.py
@@ -5,15 +5,28 @@ class Solution:
         for i in range(len(nums)):
             l = i+1
             r = len(nums)-1
-            
-            while(l < r):
-                curr_triplet_sum = sum((nums[i],nums[l],nums[r]))
-                if abs(curr_triplet_sum - target) < abs(ans - target):
-                    ans = curr_triplet_sum
-                if curr_triplet_sum > target:
-                    r-=1;
-                elif curr_triplet_sum < target:
-                    l+=1;
+            while(l<r):
+                curr_sum = nums[i]+nums[l]+nums[r]
+                if abs(curr_sum - target) < abs(ans - target):
+                    ans = curr_sum
+                elif curr_sum > target:
+                    r-=1
+                elif curr_sum < target:
+                    l+=1
                 else:
                     return ans
         return ans
+                    
+                
+                
+    '''
+Time Complexity: O(n2). We have outer and inner loops, each going through n elements.
+
+Sorting the array takes O(nlogn), so overall complexity is O(nlogn+n2). This is asymptotically equivalent to O(n2).
+
+Space Complexity: from O(logn) to O(n), depending on the implementation of the sorting algorithm.
+'''
+
+    
+    
+    
