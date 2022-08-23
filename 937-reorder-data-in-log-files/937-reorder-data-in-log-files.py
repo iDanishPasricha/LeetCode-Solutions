3865edd50aa9f417b6b06af84d954ea7b8bebce8
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-23 12:25:59 -0700

    Time: 82 ms (8.16%), Space: 14.2 MB (8.05%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.py
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.py
@@ -1,24 +1,46 @@
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
+
+        def sorting(log):
+            arr = log
+            n = len(log)
+            # basically bubble sort
+            for i in range(n): 
+            # Last i elements are already in place 
+                for j in range(0, n-i-1): 
+                    if arr[j][1:] > arr[j+1][1:]:  #The letter-logs are sorted lexicographically by their contents.
+                        arr[j], arr[j+1] = arr[j+1], arr[j]
+                    elif (arr[j][1:] == arr[j+1][1:]): 
+                        #If their contents are the same, then sort them lexicographically by their identifiers.
+                        if arr[j][0] > arr[j+1][0]:
+                            arr[j], arr[j+1] = arr[j+1], arr[j]
+            return arr
+        
+        
         Letter_log=[]
         Digit_log=[]
         
-        for log in logs:
-            
-            if(log.split()[1]).isdigit():
-                Digit_log.append(log)
+        for i in logs:
+            new_i = i.split()
+            if(new_i[1]).isdigit():
+                Digit_log.append(i)
             else:
-                Letter_log.append(log.split())
-
-        Letter_log.sort(key = lambda x:x[0])
-
-        Letter_log.sort(key = lambda x:x[1:])
-
+                Letter_log.append(new_i)
+                
+        Letter_log = sorting(Letter_log)
         
         for i in range(len(Letter_log)):
             Letter_log[i] = (" ".join(Letter_log[i]))
-        Letter_log.extend(Digit_log)
+        Letter_log.extend(Digit_log) #The letter-logs come before all digit-logs.
         return Letter_log
     
     
+    '''
+    another way
+    instead of sorting function just these 2 :
+    
+        Letter_log.sort(key = lambda x:x[0])
+        Letter_log.sort(key = lambda x:x[1:])
+    
+    '''
     
