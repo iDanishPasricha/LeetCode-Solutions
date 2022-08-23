574d8ac8404b15d714d14928f142c37aaad1bc96
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-22 20:25:10 -0700

    Time: 69 ms (85.65%), Space: 18 MB (58.11%) - LeetHub

--- 155-min-stack/155-min-stack.py
+++ 155-min-stack/155-min-stack.py
@@ -2,27 +2,27 @@ class MinStack:
     def __init__(self):
 
         self.stack= []
+        self.minstack = []
 
-    def push(self, x):
-
-        if not self.stack:
-            self.stack.append((x,x)) 
-        else:
-            self.stack.append((x,min(x,self.stack[-1][1])))
+    def push(self, val):
+        self.stack.append(val);
+        minstack_entry = min(val,self.minstack[-1] if self.minstack else val)
+        self.minstack.append(minstack_entry) 
 
     def pop(self):
 
-        if self.stack: self.stack.pop()
+        self.stack.pop()
+        self.minstack.pop()
+        
 
     def top(self):
 
-        if self.stack: return self.stack[-1][0]
-        else: return None
+        return self.stack[-1]
+        
 
     def getMin(self):
 
-        if self.stack: return self.stack[-1][1]
-        else: return None
+        return self.minstack[-1]
 
 
 '''
