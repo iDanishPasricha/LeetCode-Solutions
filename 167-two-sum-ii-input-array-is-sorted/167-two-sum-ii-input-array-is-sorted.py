35536f8d61e1510d095008e02c379f0db6a505af
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-23 14:39:27 -0700

    Time: 142 ms (86.43%), Space: 15 MB (42.04%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -11,25 +11,11 @@ class Solution:
             if nums[l]+nums[r]==target:
                 return [l+1,r+1]
             
-                
-            
-        
-        
-        
-        
-        
-        
-        
-        
-'''
-        answer=[];
-        l=0;
-        r=len(numbers)-1;
-        while(l<r):
-            if (numbers[l]+numbers[r]==target):
-                return [l+1,r+1];
-            if (numbers[l]+numbers[r]<target):
-                     l+=1;
-            else:
-                     r-=1;
-'''
+    '''
+    O(n)  --> the input array is traversed at most once
+    O(1)
+    
+    
+  What if the problem constraints were different and we needed to consider integer overflow when adding numbers[low]numbers[low] and numbers[high]numbers[high]? In that case, to prevent an overflow error, we could cast our numbers from int data type to long data type before adding them togethe
+    '''
+                  
