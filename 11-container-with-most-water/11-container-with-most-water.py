052fecd5ce756384128fef68b453ab253adcfe96
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-23 12:31:03 -0700

    Time: 1293 ms (31.03%), Space: 27.5 MB (52.27%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -1,13 +1,38 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        maxArea=  0
-        left = 0
-        right = len(height)-1
-        while left<right:
-            width  = right-left
-            maxArea = max(maxArea , min(height[left],height[right])*width)
-            if height[left]<=height[right]:
-                left+=1
-            else:
-                right-=1;
-        return maxArea
+        l = 0;
+        r = len(height)-1
+        ans = 0
+        while(l<r):
+            shorter_height = min(height[l],height[r])
+            curr_area = shorter_height*(r-l)
+            ans = max(ans,curr_area)
+            
+            if shorter_height == height[l]:
+                l+=1;
+            if shorter_height == height[r]:
+                r-=1;
+        
+        return ans
+#Time complexity: O(n). Single pass.
+
+#Space complexity: O(1). Constant space is used.
+'''
+Brute force:-
+we will simply consider the area for every possible pair of the lines and find out the maximum area out of those.
+
+T->O(n2) Calculating area for n(n-1)/2 height pairs
+S->O(1)  Constant extra space is used
+
+
+maxarea = 0
+for left in range(len(height)):
+    for right in range(left + 1, len(height)):
+        width = right - left
+        maxarea = max(maxarea, min(height[left], height[right]) * width)
+
+return maxarea
+
+'''
+        
+        
