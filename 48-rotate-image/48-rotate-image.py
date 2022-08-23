f3b82342439e52434269163c4762f6afcff2f21b
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-23 10:46:27 -0700

    Time: 35 ms (95.46%), Space: 13.9 MB (74.93%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -1,9 +1,25 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        n = len(matrix)
+        n = len(matrix) 
+        
+        # Transpose and reflect
+        
+        # Transpose
         for i in range(n):
             for j in range(i+1,n):
                 matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
+                
+        #Reflect
         for i in range(n):
             matrix[i]=matrix[i][::-1]
-        
+            
+            
+'''
+Let M be the number of cells in the grid.
+
+Time complexity : O(M) We perform two steps; transposing the matrix, and then reversing each row. Transposing the matrix has a cost of O(M) because we're moving the value of each cell once. Reversing each row also has a cost of O(M), because again we're moving the value of each cell once.
+
+Space complexity : O(1) because we do not use any other additional data structures.
+
+'''
+    
