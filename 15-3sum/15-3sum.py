2fdb9b02d2f055fa5601cdb53d87374fd76f457e
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-23 15:53:15 -0700

    Time: 635 ms (97.99%), Space: 18.1 MB (40.35%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,39 +1,70 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         
-        positive = []
-        negative = []
-        zeroes = []
+        # basically twosum + twosum II
         
-        for i in nums:
-            if i>0:
-                positive.append(i)
-            elif i<0:
-                negative.append(i)
-            else:
-                zeroes.append(i)
-                
-        P = set(positive)
-        N = set(negative)
-        ans = set()
-        
-        if zeroes:
-            for i in P:
-                if -1*i in N:
-                    ans.add((-1*i,0,i))
-        if len(zeroes)>=3:
-            ans.add((0,0,0))
+        ans = []
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i]>0: break
+            if (i>0 and nums[i]==nums[i-1]):continue
             
-        for i in range(len(negative)):
-            for j in range(i+1,len(negative)):
-                target = -1*(negative[i]+negative[j])
-                if target in P:
-                    ans.add(tuple(sorted([negative[i],negative[j],target])))
-                    
-        for i in range(len(positive)):
-            for j in range(i+1,len(positive)):
-                target = -1*(positive[i]+positive[j])
-                if target in N:
-                    ans.add(tuple(sorted([positive[i],positive[j],target])))
-                    
+            left = i+1
+            right = len(nums)-1
+            target = -1*nums[i]
+            while(left<right):
+                sum = nums[i]+nums[left]+nums[right] 
+                if sum>0:
+                    right-=1
+                elif sum<0:
+                    left+=1
+                elif sum==0:
+                    ans.append([nums[i],nums[left],nums[right]])
+                    left+=1
+                    right-=1
+                    while(left<right and nums[left]==nums[left-1]):
+                        left+=1
         return ans
+    '''
+    Time Complexity: O(n2) twoSumII is O(n), and we call it n times.
+
+Sorting the array takes O(nlogn), so overall complexity is O(nlogn+n2). This is asymptotically equivalent to O(n2) 
+
+Space Complexity: from O(logn) to O(n), depending on the implementation of the sorting algorithm. For the purpose of complexity analysis, we ignore the memory required for the output.
+
+Another approach using hashset
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                break
+            if i == 0 or nums[i - 1] != nums[i]:
+                self.twoSum(nums, i, res)
+        return res
+
+    def twoSum(self, nums: List[int], i: int, res: List[List[int]]):
+        seen = set()
+        j = i + 1
+        while j < len(nums):
+            complement = -nums[i] - nums[j]
+            if complement in seen:
+                res.append([nums[i], nums[j], complement])
+                while j + 1 < len(nums) and nums[j] == nums[j + 1]:
+                    j += 1
+            seen.add(nums[j])
+            j += 1
+    
+Time Complexity: O(n2). twoSum is O(n), and we call it n times.
+
+Sorting the array takes O(nlogn), so overall complexity is O(nlogn+n2) . This is asymptotically equivalent  O(n2).
+
+Space Complexity:O(n) for the hashset.
+
+'''
+
+        
+                
+        
