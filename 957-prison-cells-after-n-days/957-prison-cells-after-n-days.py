2994b140152fb8724a5ac9b4a71d695d2cc013d3
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-24 13:36:05 -0700

    Time: 65 ms (44.37%), Space: 13.9 MB (27.49%) - LeetHub

--- 957-prison-cells-after-n-days/957-prison-cells-after-n-days.py
+++ 957-prison-cells-after-n-days/957-prison-cells-after-n-days.py
@@ -10,7 +10,7 @@ class Solution:
             return temp_array
         
         
-        
+        # cycle detection
         if len(cells)==0 or n<=0: return cells
         visited = set()
         hasCycle = False
@@ -30,6 +30,27 @@ class Solution:
             for i in range(n):
                 cells = nextDay(cells)
         return cells
+    
+    
+'''
+Let K be the number of cells, and N be the number of steps.
+
+Time Complexity: O(K.min(N,2^K))
+
+As we discussed before, at most we could have 2^K possible states. While we run the simulation with N steps, we might need to run min(N, 2^K) steps without fast-forwarding in the worst case.
+
+For each simulation step, it takes O(K) time to process and evolve the state of cells.
+
+Hence, the overall time complexity of the algorithm is O(K.min(N,2^K))
+
+
+Space Complexity:
+
+The main memory consumption of the algorithm is the hashmap that we used to keep track of the states of the cells. The maximal number of entries in the hashmap would be 2^K as we discussed before.
+
+In the Python implementation, we keep the states of cells as they are in the hashmap. As a result, for each entry, it takes O(K) space. In total, its space complexity becomes O(K⋅2^K)
+
+ '''
             
             
         
