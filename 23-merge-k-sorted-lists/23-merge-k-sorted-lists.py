6e54800e389d7e04ab9872f8205ab7d47f5eb156
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-24 09:52:27 -0700

    Time: 201 ms (34.51%), Space: 17.6 MB (94.08%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -1,6 +1,6 @@
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        def merge_2_linked_lists(l1,l2):
+        def merge2Lists(l1,l2):
             start = ListNode(0);
             head = start
             while l1 and l2:
@@ -15,6 +15,26 @@ class Solution:
             
             return head.next
         
+        if not lists or len(lists)==0: return None
+        
+        while len(lists)>1: #until we have just one list
+            temp_lists = []
+            for i in range(0,len(lists),2): #2 for pairing
+                l1 = lists[i]
+                l2 = lists[i+1] if (i+1)<len(lists) else None #check if it is not out of bounce
+                temp_lists.append(merge2Lists(l1,l2))
+            lists = temp_lists
+        return lists[0]
+        
+
+        '''We can merge two sorted linked list in O(n) time where nn is the total number of nodes in two lists.
+           Sum up the merge process and we can get: O(Nlogk)
+           Space complexity : O(1)We can merge two sorted linked lists in O(1) space.
+        '''
+        '''
+        or write this 
+        
         if len(lists)==0: return 
         if len(lists)==1: return lists[0]
         else: return merge_2_linked_lists(self.mergeKLists(lists[:len(lists)//2]),self.mergeKLists(lists[len(lists)//2:]))
+'''
