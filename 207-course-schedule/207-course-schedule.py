badad0df54938d829acdd67c4d0a1ba708b46039
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-24 22:17:12 -0700

    Time: 184 ms (30.60%), Space: 17.2 MB (25.94%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -6,12 +6,12 @@ class Solution:
             premap[crs].append(pre)
         visited = set()
         def dfs(crs):
-            if crs in visited: return False
-            if premap[crs]==[]: return True
+            if crs in visited: return False # its a cycle--> [[1,0],[0,1]]
+            if premap[crs]==[]: return True  # all prequisites of that course clear
             
             visited.add(crs)
             for prereqs in premap[crs]:
-                if not dfs(prereqs): return False
+                if not dfs(prereqs): return False  #if we find just 1 course only which can't be completed i.e it has a loop then we will immediately return False
 
             visited.remove(crs)
             premap[crs] = []
