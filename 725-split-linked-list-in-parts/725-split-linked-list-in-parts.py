c2818559b055c33bc7e9bd0807291ca6702560ff
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-27 17:30:10 -0700

    Time: 64 ms (41.24%), Space: 14.4 MB (38.37%) - LeetHub

--- 725-split-linked-list-in-parts/725-split-linked-list-in-parts.py
+++ 725-split-linked-list-in-parts/725-split-linked-list-in-parts.py
@@ -7,10 +7,13 @@ class Solution:
     def splitListToParts(self, head: Optional[ListNode], k: int) -> List[Optional[ListNode]]:
         count = 0;
         temp=head;
+        # step1:- count the length
         while temp:
             count+=1;
             temp = temp.next;
         
+        #step2:- count how many equal and extra parts we can make
+        #ex:- [1,2,3,4,5,6,7,8,9,10] equal parts = 3, extra = 1
         equals = count//k 
         extras = count%k;
         
