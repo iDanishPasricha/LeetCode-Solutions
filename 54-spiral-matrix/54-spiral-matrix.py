8845356b0c59b55841c4a69607061b081125395c
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-27 19:16:11 -0700

    Time: 32 ms (92.48%), Space: 14.1 MB (33.75%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -11,24 +11,24 @@ class Solution:
         
         
         ans=[];
-        
         while left_column<=right_column and top_row<=bottom_row:
             for i in range(left_column,right_column+1):
-                ans.append(matrix[top_row][i]);
-            top_row+=1;
+                ans.append(matrix[top_row][i])
+            top_row+=1
             for i in range(top_row,bottom_row+1):
-                ans.append(matrix[i][right_column]);
-            right_column-=1;
+                ans.append(matrix[i][right_column])
+            right_column-=1
             
-            if left_column>right_column or top_row>bottom_row: break;
+            if left_column>right_column or top_row>bottom_row: break
                 
             for i in range(right_column,left_column-1,-1):
-                ans.append(matrix[bottom_row][i]);
-            bottom_row-=1;
+                ans.append(matrix[bottom_row][i])
+            bottom_row-=1
             for i in range(bottom_row,top_row-1,-1):
-                ans.append(matrix[i][left_column]);
-            left_column+=1;
+                ans.append(matrix[i][left_column])
+            left_column+=1
         return ans
+            
                 
             
         
