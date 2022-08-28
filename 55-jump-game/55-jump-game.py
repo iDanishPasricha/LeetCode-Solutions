197acf7501c4327bf7507b13310f9e8de88d0e64
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-27 19:59:15 -0700

    Time: 1035 ms (18.32%), Space: 15.3 MB (49.29%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -10,4 +10,22 @@ class Solution:
             jump = max(jump,i+nums[i])
       
     
+    '''
+    another approach greedy
+    Case 1:- [2,3,1,1,4] --> True
+    Case 2:- [3,2,1,0,4] --> False
+    Try with both cases
+    last = n-1
+    for ( i= n-2,i>=0;i--)
+    {
+        if (nums[i]+i) >= last
+        {
+            last = i
+        }
+    }
+    return last<=0
+    
+    '''
+    
+    
     
