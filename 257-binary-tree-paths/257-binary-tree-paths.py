d9de6086334adc564b6b98aff94a7e19517a96ae
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-27 18:00:17 -0700

    Time: 39 ms (82.67%), Space: 13.9 MB (74.92%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.py
+++ 257-binary-tree-paths/257-binary-tree-paths.py
@@ -7,17 +7,17 @@
 class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
         if not root: return []
-        ans=[];
         stack = [(root,"")]
-        
+        ans = []
         while stack:
-            node,string = stack.pop();
+            node,string = stack.pop()
             if not node.left and not node.right:
                 ans.append(string+str(node.val))
-                
             if node.left:
                 stack.append((node.left,string+str(node.val)+"->"))
-            if node.right:
+                
+            if  node.right:
                 stack.append((node.right,string+str(node.val)+"->"))
         return ans
+            
                 
