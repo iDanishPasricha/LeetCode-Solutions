e2c9ad6bf0e449ebd5d43e06029775a568610543
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-27 20:21:05 -0700

    Time: 60 ms (45.86%), Space: 13.9 MB (80.88%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.py
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.py
@@ -1,21 +1,17 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-        
-        count = Counter(s)
-        visited = set()
+        d = Counter(s)
         stack = []
-
+        visited = set()
         for i in s:
-            if i not in visited:  
-                while stack and stack[-1] > i and count[stack[-1]] > 0:
-                    print("stack before",stack)
-                    visited.remove(stack.pop()) 
-                    print("stack after",stack)
+            if i not in visited:
+                while stack and stack[-1]>i and d[stack[-1]]>0:
+                    visited.remove(stack.pop())
                 stack.append(i)
                 visited.add(i)
-            count[i]-= 1
-            
-        return "".join(stack)
+            d[i]-=1
+        return ''.join(stack)
+                
     
     
     #while loop conditions --stack jinda hai na empty ta nhi   --lexicographical order maintained hai {a,b,c,d,e aivein like dictionary}  --d[digit] mukk te nhi gya kitte halle agge v aana hai na ,last occurence ta nhi c ehdi
