8400301cb80c3b13681f354704a8a86ccf9049bc
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-28 17:10:07 -0700

    Time: 149 ms (65.24%), Space: 14.3 MB (93.91%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
@@ -3,7 +3,9 @@ class Solution:
         ans=[] 
         people.sort(key=lambda x: (-x[0], x[1]))
     
-        
+        #x[0] == height and x[1] == k value (h,k)
+    # sort the array in decreasing order of height 
+    # within the same height group, you would sort it in increasing order of k
         for i in people:
             
             ans.insert(i[1], i)
@@ -14,8 +16,7 @@ class Solution:
             # k will act as a position within the array
     
     
-    # sort the array in decreasing order of height 
-    # within the same height group, you would sort it in increasing order of k
+
     # eg: Input : [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
     # after sorting: [[7,0],[7,1],[6,1],[5,0],[5,2],[4,4]]
     
