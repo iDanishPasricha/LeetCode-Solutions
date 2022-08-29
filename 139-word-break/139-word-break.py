138f782660f420c0e585cd8948ca820d02a303c6
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-29 11:29:55 -0700

    Time: 38 ms (95.12%), Space: 14 MB (70.81%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -10,3 +10,33 @@ class Solution:
                     dp[i] = dp[i+len(word)]
                 if dp[i]: break
         return dp[0]
+    
+'''
+just explain it with example s =  Catsanddog and wordDict = ["Cat","Cats","Sand","And","Dog"]
+
+word_set = set(wordDict)
+q = deque()
+visited = set()
+q.append(0)
+
+while q:
+    start = q.popleft()
+    if start in visited:
+        continue
+    for end in range(start + 1, len(s) + 1):
+        if s[start:end] in word_set:
+            q.append(end)
+            if end == len(s):
+                return True
+    visited.add(start)
+return False
+
+
+#Complexity
+n is the length of the input string.
+
+Time complexity : O(n^3) For every starting index, the search can continue till the end of the given string.
+
+Space complexity : O(n). Queue of at most n size is needed.
+
+'''
