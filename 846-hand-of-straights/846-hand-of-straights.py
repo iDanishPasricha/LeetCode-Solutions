63ce54f340c916be18991bd439470e1d2115794f
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-28 18:33:23 -0700

    Time: 365 ms (40.34%), Space: 15.7 MB (46.01%) - LeetHub

--- 846-hand-of-straights/846-hand-of-straights.py
+++ 846-hand-of-straights/846-hand-of-straights.py
@@ -1,27 +1,24 @@
+import heapq
 class Solution:
     def isNStraightHand(self, nums: List[int], k: int) -> bool:
-        def func(nums):
-            start = min(nums)
-            l=[start];
-            nums.remove(start)
-            i=1
-            while(i<k):
-                if (start+i) in nums:
-                    l.append(start+i)
-                    nums.remove(start+i)
-                else: return [78]
-                i+=1;
-            print(l)
-
-            return nums
         if len(nums)%k!=0: return False
-        else:
-            
-            while(len(nums)):
-                new_nums = func(nums)
-                if new_nums==[78]:return False
-                if len(new_nums)==0:break
-                else:
-                    nums=new_nums
-            return True
+        d={};
+        
+        for i in range(len(nums)):
+            if nums[i] not in d:
+                d[nums[i]]=1
+            else:
+                d[nums[i]]+=1;
+        
+        minHeap=[i for i in d.keys()]
+        heapq.heapify(minHeap)
+        while minHeap:
+            first = minHeap[0]
+            for i in range(first,first+k):
+                if i not in d: return False
+                d[i]-=1;
+                if d[i]==0:
+                    if i!=minHeap[0]: return False  #EX"- [1,2,3,6,2,3,4,7,3,8,8,5] AND groupSize = 3
+                    heapq.heappop(minHeap)
+        return True
         
