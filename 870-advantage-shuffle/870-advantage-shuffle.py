354dab4e018ea489152e29aabcc5f8b1d0eef34e
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-08-29 14:43:34 -0700

    Time: 1935 ms (11.13%), Space: 31.8 MB (96.14%) - LeetHub

--- 870-advantage-shuffle/870-advantage-shuffle.py
+++ 870-advantage-shuffle/870-advantage-shuffle.py
@@ -1,7 +1,9 @@
 class Solution:
     def advantageCount(self, nums1: List[int], nums2: List[int]) -> List[int]:
         def helper(idx):
-            left, right = 0, len(nums1)-1
+            left  = 0; 
+            right = len(nums1)-1
+            
             while left <= right:
                 mid = (left + right) // 2
                 if nums1[mid] > nums2[idx]:
@@ -16,7 +18,7 @@ class Solution:
             nums1.pop(temp)
             
         nums1.sort()
-        visited = [False] * len(nums1)
+
         ans = []
         for i in range(len(nums2)):
             helper(i)
