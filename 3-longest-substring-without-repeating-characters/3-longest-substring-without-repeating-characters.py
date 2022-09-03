4aababa2cfc7c66ec76794de5dc37c661c899ea0
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-09-02 18:51:33 -0700

    Time: 296 ms (19.33%), Space: 14 MB (49.69%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,14 +1,36 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        ans=[];
-        left = right = 0;
+        left = 0;
+        right = 0;
         maxLen = 0;
         
-        while(right<len(s)):
+        while(right < len(s)):
 
-            if len(set(s[left:right+1])) < len(s[left:right+1]):
-                while(len(set(s[left:right+1]))!=len(s[left:right+1])):
-                    left+=1;
-            maxLen = max(maxLen,right-left+1)
-            right+=1
+            while(len(set(s[left:right+1]))!= len(s[left:right+1])):
+                  left+=1;
+            maxLen = max(maxLen , right-left+1);
+            right+=1;
         return maxLen
+    
+    
+    
+    
+# Time complexity : O(2n)=O(n). In the worst case each character will be visited twice by i and j.
+
+# same complextity = same as brute foce O(min(m,n))
+
+    
+    ''' 
+    Brute force
+    Intuition
+
+Check all the substring one by one to see if it has no duplicate character.
+
+Time complexity : O(n^3)
+
+
+Space complexity : O(min(n,m)). We need O(k) space for checking a substring has no duplicate characters, where k is the size of the Set. The size of the Set is upper bounded by the size of the string n and the size of the charset/alphabet m.
+'''
+
+        
+        
