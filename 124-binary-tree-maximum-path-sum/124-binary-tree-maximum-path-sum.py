be667eee91af214bd79a4e4a0de3c5c913ced2f4
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-09-07 13:44:20 -0700

    Time: 203 ms (6.63%), Space: 21.7 MB (10.40%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
@@ -14,13 +14,22 @@ class Solution:
             ls = func(root.left);
             rs = func(root.right);
 
+            case1 = root.val
+            case2 = root.val + max(ls,rs)
+            case3 = root.val + ls + rs
             
+            curr_ans = max(case1,case2,case3)
+            ans      = max(curr_ans,ans)
+            
+            return max(case1,case2)
+        '''
             temp = max(root.val + (max(ls,rs)),root.val) # max(case2,case3)
             curr_ans = max(temp,root.val+ls+rs) #max(temp,case1)
             ans = max(curr_ans,ans)
+    
 
             return temp  # we are returning temp because in case 2 either we will pass through LS or RS if we return ans and suppose case 1 is maximum i.e (RD+LS+RS) is stored in ans variable ,then whole tree will be included and whole tree will be returned
-        
+        '''
         func(root);
         return ans
     
