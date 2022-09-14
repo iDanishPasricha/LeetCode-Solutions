4d66738a56980859459ab4312168b9bb3677c79e
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-09-14 14:45:08 -0700

    Time: 2886 ms (5.38%), Space: 17.9 MB (51.34%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.py
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.py
@@ -4,22 +4,25 @@ class Solution:
         nums = nums + nums
         l = 0
         r = 0
-        curr_window_length = 0
-        max_window_length = 0
+        curr_window = 0
+        max_window_length  = 0
         
         while r < len(nums):
             
-            if r-l < window_width:
-                curr_window_length += nums[r]
-                
-                r += 1
-            else:
-                curr_window_length -= nums[l]
-                curr_window_length += nums[r]
-                
+            curr_window += nums[r]
+            r += 1
+  
+            if r - l > window_width:
+                curr_window -= nums[l]
                 l += 1
-                r += 1
-            max_window_length = max(max_window_length,curr_window_length)
+                
+                
+            max_window_length = max(max_window_length,curr_window)
                 
         
         return window_width - max_window_length
+    
+    
+    
+    
+    
