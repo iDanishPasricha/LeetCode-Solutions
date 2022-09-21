0b56bea09e0e253e42fb3250916d9189321d5d29
Danish Pasricha <94999606+DanishPasricha@users.noreply.github.com>
2022-09-20 18:33:46 -0700

    Time: 204 ms (11.40%), Space: 21.3 MB (62.08%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
@@ -7,34 +7,22 @@
 import math
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        ans = [root.val]
+        ans = float("-inf")
+        def func(root):
+            nonlocal ans
+            if root == None: return 0
+            ls = max(func(root.left), 0)
+            rs = max(func(root.right),0)
+
+            
+            temp = root.val + max(ls,rs) # max(case2,case3)
+            curr_ans = max(temp,root.val+ls+rs) #max(temp,case1)
+            ans = max(curr_ans,ans)
+
+            return temp  # we are returning temp because in case 2 either we will pass through LS or RS if we return ans and suppose case 1 is maximum i.e (RD+LS+RS) is stored in ans variable ,then whole tree will be included and whole tree will be returned
         
-        #remember if we are starting at a node we can only split once because if we violate this condition them it will no longer be a path
-        
-        
-        #return sum with considering no split allowed 
-        def dfs(root):
-            if not root: return 0
-            
-            leftMax  = dfs(root.left)
-            rightMax = dfs(root.right)
-            
-            leftMax  = max(leftMax,0)  #handling negative cases if leftmax is -ve then wont take it
-            rightMax = max(rightMax,0) #handling negative cases if rigtmax is -ve then wont take it
-            
-            #compute maxSum WITH split (split allowed we can go in both directions left and right from root)
-            
-            ans[0] = max(ans[0] , leftMax + rightMax + root.val)
-            
-            return root.val + max(leftMax,rightMax)
-            
-            
-        '''
-A node can only appear in the sequence at most once. 
-we are returning max(case1,case2) because in case 2 either we will pass through LS or RS if we return ans and suppose case 1 is maximum i.e (RD+LS+RS) is stored in ans variable ,then whole tree will be included and whole tree will be returned
-        '''
-        dfs(root);
-        return ans[0]
+        func(root);
+        return ans
     
     #case 1:- rootdata>0 , LS>0 , RS>0 --> maxsum = RD+LS+RS
     #case 2:- (LS<0 , RS>0) or (LS>0 , RS<0) --> maxsum = RD + max(LS,RS)
